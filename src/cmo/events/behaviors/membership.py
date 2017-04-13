801feb50327945cb0c277a8a4a30337d7fa1c79a
adriana-rv <adriana.rv@gmail.com>
2017-04-12 23:33:53 -0500

    clean code

--- src/cmo/events/behaviors/membership.py
+++ src/cmo/events/behaviors/membership.py
@@ -1,19 +1,13 @@
 # -*- coding: utf-8 -*-
 from cmo.events import _
 from plone.autoform.interfaces import IFormFieldProvider
-# from plone.directives import form
-# from plone.autoform import directives
+from plone.dexterity.interfaces import IDexterityContent
 from plone.supermodel import model
 from zope import schema
-# from zope.interface import alsoProvides
-# from plone.supermodel.directives import fieldset
-
 from zope.component import adapter
 from zope.interface import implementer
-from plone.dexterity.interfaces import IDexterityContent
-
-
 from zope.interface import provider
+
 
 @provider(IFormFieldProvider)
 class IMembership(model.Schema):
@@ -108,12 +102,10 @@ class IMembership(model.Schema):
         required=False,
     )
 
+
 @implementer(IMembership)
 @adapter(IDexterityContent)
 class Membership(object):
 
     def __init__(self, context):
         self.context = context
-    
-
-# alsoProvides(IMembership, IFormFieldProvider)
