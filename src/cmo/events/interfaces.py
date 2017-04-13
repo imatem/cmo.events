5fff6fdf56855fdca5f9c948df9d04054da3633b
adriana-rv <adriana.rv@gmail.com>
2017-04-12 23:18:29 -0500

    Add types for fields and update fields

--- src/cmo/events/interfaces.py
+++ src/cmo/events/interfaces.py
@@ -43,24 +43,49 @@ class IParticipant(Interface):
 class IWorkshop(Interface):
     """Workshop
     """
+    
+    code = schema.TextLine(
+        title=_(u'label_cmo_workshop_code', u'Workshop ID'),
+        required=False,
+    )
+
     # form.mode(title='hidden')
     title = schema.TextLine(
-        title=_(u'Title'),
+        title=_(u'label_cmo_workshop_name', u'Workshop Name'),
         required=False,
     )
 
-    workshopid = schema.TextLine(
-        title=_(u'label_cmo_workshopid', u'Workshop ID'),
+    shortname = schema.TextLine(
+        title=_(u'Shorname'),
         required=False,
     )
 
-    workshopstart = schema.TextLine(
-        title=_(u'label_cmo_workshopstart', u'Workshop Start Date'),
+    start_date = schema.Date(
+        title=_(u'label_cmo_workshop_start_date', u'Workshop Start Date'),
         required=False,
     )
 
-    workshopend = schema.TextLine(
-        title=_(u'label_cmo_workshopend', u'Workshop End Date'),
+    end_date = schema.Date(
+        title=_(u'label_cmo_workshop_end_date', u'Workshop End Date'),
+        required=False,
+    )
+
+    workshop_type = schema.TextLine(
+        title=_(u'label_cmo_workshop_type', u'Workshop Type'),
+        required=False,
+    )
+    max_participants = schema.Int(
+        title=_(u'label_cmo_workshop_max_participants', u'Maximum Number of Participants'),
+        required=False,
+    )
+
+    description = schema.Text(
+        title=_(u'label_cmo_workshop_description', u'Workshop Description'),
+        required=False,
+    )
+
+    press_realease = schema.TextLine(
+        title=_(u'label_cmo_workshop_press_release', u'Press Release'),
         required=False,
     )
 
