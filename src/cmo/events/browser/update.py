a9cd7580fdfb2877f4d38f2d7d5fe9d5cd624dc6
gil-cano <gil.cano@gmail.com>
2017-04-14 18:02:12 -0500

    update participants

--- src/cmo/events/browser/update.py
+++ src/cmo/events/browser/update.py
@@ -12,6 +12,19 @@ import requests
 
 logger = logging.getLogger('Plone')
 
+headers = {
+    'Host': 'www.birs.ca',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0',  # noqa
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  # noqa
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Authorization': 'Basic Ymlyc3dzOkcwcjFsbDRfU2wxcHAzcno=',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
+
 
 class UpdateWorshopsForm(form.Form):
     """Update workshops from Birs DB."""
@@ -21,22 +34,9 @@ class UpdateWorshopsForm(form.Form):
         """Update workshops list
         """
         logger.info('Updating Workshops')
-        # members/17w5060
         year = str(2017)
         birs_uri = api.portal.get_registry_record('cmo.birs_api_uri')
         url = '/'.join([birs_uri, 'event_data_for_year', year])
-        headers = {
-            'Host': 'www.birs.ca',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0',  # noqa
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  # noqa
-            'Accept-Language': 'en-US,en;q=0.5',
-            'Accept-Encoding': 'gzip, deflate, br',
-            'Connection': 'keep-alive',
-            'Upgrade-Insecure-Requests': '1',
-            'Authorization': 'Basic Ymlyc3dzOkcwcjFsbDRfU2wxcHAzcno=',
-            'Pragma': 'no-cache',
-            'Cache-Control': 'no-cache',
-        }
         try:
             req = requests.get(url, headers=headers)
             req.raise_for_status()
@@ -67,3 +67,35 @@ class UpdateWorshopsForm(form.Form):
                     title=item['name'],
                     container=folder,
                     **item)
+
+
+class UpdateParticipantsForm(form.Form):
+    """Update Participants from Birs DB."""
+
+    @button.buttonAndHandler(u'Update participants')
+    def handle_update_participants(self, action):
+        """Update participants list
+        """
+        logger.info('Updating participants for {0}'.format(self.context.id))
+        birs_uri = api.portal.get_registry_record('cmo.birs_api_uri')
+        url = '/'.join([birs_uri, 'members', self.context.id])
+        try:
+            req = requests.get(url, headers=headers)
+            req.raise_for_status()
+        except (ConnectionError, HTTPError) as err:
+            api.portal.show_message(err, self.request, type=u'error')
+        else:
+            self.update_participants(req.json())
+            api.portal.show_message(u'Updated!', self.request, type=u'info')
+        logger.info('Done.')
+
+    def update_participants(self, json_data):
+        """
+        """
+        for item in json_data:
+            if item['Person']['email'] not in self.context:
+                api.content.create(
+                    type='Participant',
+                    id=item['Person']['email'],
+                    title=item['Person']['firstname'],
+                    container=self.context)
