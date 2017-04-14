ef5e51a5b720925034dab06b73b570d99f1a8e0f
gil-cano <gil.cano@gmail.com>
2017-04-14 17:06:12 -0500

    create workshop content

--- src/cmo/events/browser/update.py
+++ src/cmo/events/browser/update.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 
+from datetime import datetime
 from plone import api
-from unidecode import unidecode
+from requests.exceptions import ConnectionError
+from requests.exceptions import HTTPError
 from z3c.form import button
 from z3c.form import form
 
-import json
 import logging
 import requests
 
@@ -13,22 +14,21 @@ logger = logging.getLogger('Plone')
 
 
 class UpdateWorshopsForm(form.Form):
-    """Update workshops from Birs."""
+    """Update workshops from Birs DB."""
 
     @button.buttonAndHandler(u'Update Workshops')
     def handle_update_workshops(self, action):
+        """Update workshops list
         """
-        """
-        logger.info("Updating Workshops")
-        # event_data_for_year/2017
+        logger.info('Updating Workshops')
         # members/17w5060
         year = str(2017)
         birs_uri = api.portal.get_registry_record('cmo.birs_api_uri')
         url = '/'.join([birs_uri, 'event_data_for_year', year])
         headers = {
             'Host': 'www.birs.ca',
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0',
-            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0',  # noqa
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  # noqa
             'Accept-Language': 'en-US,en;q=0.5',
             'Accept-Encoding': 'gzip, deflate, br',
             'Connection': 'keep-alive',
@@ -37,7 +37,33 @@ class UpdateWorshopsForm(form.Form):
             'Pragma': 'no-cache',
             'Cache-Control': 'no-cache',
         }
-        req = requests.get(url, headers=headers)
-        logger.info(url)
-        logger.info(req.status_code)
-        logger.info("Done.")
+        try:
+            req = requests.get(url, headers=headers)
+            req.raise_for_status()
+        except (ConnectionError, HTTPError) as err:
+            api.portal.show_message(err, self.request, type=u'error')
+        else:
+            self.update_workshops(year, req.json())
+            api.portal.show_message(u'Updated!', self.request, type=u'info')
+        logger.info('Done.')
+
+    def update_workshops(self, year, json_data):
+        """Update workshops for year
+        """
+        folder = api.content.get(path='/{0}'.format(year))
+        if not folder:
+            folder = api.content.create(
+                type='Folder',
+                title=year,
+                container=api.portal.get())
+        for item in json_data:
+            if item['code'] not in folder:
+                item['start_date'] = datetime.strptime(item['start_date'], '%Y-%m-%d')  # noqa
+                item['end_date'] = datetime.strptime(item['end_date'], '%Y-%m-%d')  # noqa
+                item['max_participants'] = int(item['max_participants'])
+                api.content.create(
+                    type='Workshop',
+                    id=item['code'],
+                    title=item['name'],
+                    container=folder,
+                    **item)
