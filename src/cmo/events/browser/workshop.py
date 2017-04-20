949d6ab7d23bb40a69d205d5ac041051f84b2d11
adriana-rv <adriana.rv@gmail.com>
2017-04-20 16:25:02 -0500

    Add update button

--- src/cmo/events/browser/workshop.py
+++ src/cmo/events/browser/workshop.py
@@ -1,8 +1,30 @@
 # -*- coding: utf-8 -*-
+from cmo.events import _
+from datetime import datetime
+from plone import api
 from plone.autoform.view import WidgetsView
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import getAdditionalSchemata
+from requests.exceptions import ConnectionError
+from requests.exceptions import HTTPError
 from zope.component import getUtility
+
+import logging
+import requests
+
+logger = logging.getLogger('Plone')
+headers = {
+    'Host': 'www.birs.ca',
+    'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.11; rv:52.0) Gecko/20100101 Firefox/52.0',  # noqa
+    'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',  # noqa
+    'Accept-Language': 'en-US,en;q=0.5',
+    'Accept-Encoding': 'gzip, deflate, br',
+    'Connection': 'keep-alive',
+    'Upgrade-Insecure-Requests': '1',
+    'Authorization': 'Basic Ymlyc3dzOkcwcjFsbDRfU2wxcHAzcno=',
+    'Pragma': 'no-cache',
+    'Cache-Control': 'no-cache',
+}
 
 
 class WorkshopView(WidgetsView):
@@ -23,6 +45,10 @@ class WorkshopView(WidgetsView):
     def __call__(self):
         # utility function to add resource to rendered page
         # add_resource_on_request(self.request, 'tablecmo')
+        # import pdb; pdb.set_trace()
+        if self.request.form:
+            self.handle_update_participants()
+
         return super(WorkshopView, self).__call__()
 
     def participants(self):
@@ -79,3 +105,39 @@ class WorkshopView(WidgetsView):
 
             participants['rows'].append(row)
         return participants
+
+    def handle_update_participants(self):
+        """Update participants list
+        """
+        logger.info('Updating participants for {0}'.format(self.context.id))
+        birs_uri = api.portal.get_registry_record('cmo.birs_api_uri')
+        url = '/'.join([birs_uri, 'members', self.context.id])
+        try:
+            req = requests.get(url, headers=headers)
+            req.raise_for_status()
+        except (ConnectionError, HTTPError) as err:
+            api.portal.show_message(err, self.request, type=u'error')
+        else:
+            self.update_participants(req.json())
+            api.portal.show_message(_(u'Updated!'), self.request, type=u'info')
+        logger.info('Done.')
+
+    def update_participants(self, json_data):
+        """Update participants list
+        """
+        for item in json_data:
+            if item['Person']['email'] not in self.context:
+                kargs = dict(item['Person'])
+                kargs.update(item['Membership'])
+                kargs['workshop'] = item['Workshop']
+                for d in ['arrival_date', 'replied_at', 'departure_date']:
+                    if kargs[d] is not None:
+                        kargs[d] = datetime.strptime(kargs[d], '%Y-%m-%d %H:%M:%S')  # noqa
+                    else:
+                        del kargs[d]
+                api.content.create(
+                    type='Participant',
+                    id=kargs['email'],
+                    title=kargs['firstname'],
+                    container=self.context,
+                    **kargs)
