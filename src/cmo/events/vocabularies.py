ec020bdb2980025e31c58c7fa16662e69461e311
adriana-rv <adriana.rv@gmail.com>
2017-05-25 16:30:32 -0500

    Get all templates

--- src/cmo/events/vocabularies.py
+++ src/cmo/events/vocabularies.py
@@ -2,18 +2,23 @@
 
 from zope.interface import provider
 # from Products.CMFCore.utils import getToolByName
-# from zope.component.hooks import getSite
+from zope.component.hooks import getSite
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleVocabulary
 
 
 @provider(IVocabularyFactory)
 def CertificatesTemplates(context):
+    """Get all certificates templates in home site"""
 
     templates = []
+    alltemplates = []
 
-    for item in ['Latex Template', ]:
+    folder_templates = [item for item in getSite().values() if item.id == 'templates-for-certificates' and item.portal_type == 'Folder']
+    if folder_templates:
+        alltemplates = folder_templates[0].values()
+
+    for template in alltemplates:
         templates.append(SimpleVocabulary.createTerm(
-            item.replace(' ', '-').lower(), item.replace(' ', '-').lower(), item))
-
+            template.UID(), template.id, template.title))
     return SimpleVocabulary(templates)
