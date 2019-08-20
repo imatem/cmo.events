be0740554a467a0e7bdecdab73eff9b565b08372
adriana-rv <adriana.rv@gmail.com>
2019-08-20 18:55:07 -0500

    Update two fields information in birs

--- src/cmo/events/browser/participant.py
+++ src/cmo/events/browser/participant.py
@@ -6,6 +6,18 @@ from plone.dexterity.utils import getAdditionalSchemata
 from plone.registry.interfaces import IRegistry
 from zope.component import getUtility
 # from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+from z3c.form import button
+from Products.statusmessages.interfaces import IStatusMessage
+from zope.event import notify
+from plone.dexterity.events import EditFinishedEvent
+from plone.dexterity.i18n import MessageFactory as _
+
+import logging
+import requests
+from plone import api
+from requests.exceptions import ConnectionError
+from requests.exceptions import HTTPError
+logger = logging.getLogger('Plone')
 
 
 class ParticipantEditView(DefaultEditForm):
@@ -21,6 +33,61 @@ class ParticipantEditView(DefaultEditForm):
             if portal_type in use_view_action:
                 view_url = view_url + '/view'
         return view_url
+
+        # api user test password
+
+    @button.buttonAndHandler(_(u'Save'), name='save')
+    def handleApply(self, action):
+        data, errors = self.extractData()
+        if errors:
+            self.status = self.formErrorsMessage
+            return
+        self.applyChanges(data)
+        IStatusMessage(self.request).addStatusMessage(
+            self.success_message, "info"
+        )
+        self.request.response.redirect(self.nextURL())
+
+        logger.info('Message {0}'.format(self.context.id))
+        birs_uri = api.portal.get_registry_record('cmo.birs_api_uri')
+        email_autho = api.portal.get_registry_record('cmo.birs_api_user')
+        passwd_autho = api.portal.get_registry_record('cmo.birs_api_password')
+
+        try:
+            token = requests.post(
+                birs_uri + '/api/login.json',
+                headers={
+                    'Accept': 'application/json',
+                    'Content-Type': 'application/json',
+                },
+                json={'api_user': {'email': email_autho, 'password': passwd_autho}},
+            )
+
+            jwt = 'Bearer ' + token.json()['jwt']
+            workshop_number = self.context.aq_parent.id
+
+            birs_url_membership = '%s/events/%s/memberships/%s.json' % (birs_uri, workshop_number, data['IMembership.birs_membership_id'])
+
+            req = requests.patch(
+                birs_url_membership,
+                headers={'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': jwt},
+                json={
+                    'membership': {
+                        'staff_notes': data['IMembership.event_notes'],
+                        'person_attributes': {
+                            'title': data['IPerson.birs_title']
+                        }
+                    }
+                },
+            )
+            req.raise_for_status()
+        except (ConnectionError, HTTPError) as err:
+            api.portal.show_message(err, self.request, type=u'error')
+        # else:
+
+            # self.update_participants(req.json())
+
+        notify(EditFinishedEvent(self.context))
 
 
 class ParticipantView(WidgetsView):
