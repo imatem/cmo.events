bd222125ab15cd7fd724bd836e4fe9611cac64de
gil-cano <gil.cano@gmail.com>
2019-08-20 15:16:59 -0500

    update control panel settings

--- src/cmo/events/browser/controlpanel.py
+++ src/cmo/events/browser/controlpanel.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from cmo.events import _
 from plone.app.registry.browser.controlpanel import ControlPanelFormWrapper
 from plone.app.registry.browser.controlpanel import RegistryEditForm
 from plone.z3cform import layout
@@ -9,22 +10,48 @@ from zope.interface import Interface
 class ICMOControlPanel(Interface):
 
     birs_api_uri = schema.URI(
-        title=u'Birs API url',
+        title=_(
+            u'label_birs_api_url',
+            default=u'BIRS whorkshops API url'),
+        description=_(
+            u'help_birs_api_url',
+            default=u'e.g. https://staging.birs.ca'),
+        default=None,
         required=True,
     )
 
     birs_api_user = schema.TextLine(
-        title=u'Birs API user',
+        title=_(
+            u'label_birs_api_user',
+            default=u'BIRS whorkshops API user'),
+        description=_(
+            u'help_birs_api_user',
+            default=u'Username for authentication '
+                    u'to the BIRS whorkshops API'),
+        default=None,
         required=False,
     )
 
     birs_api_password = schema.Password(
-        title=u'Birs API password',
-        required=False,
-    )
+        title=_(
+            u'label_birs_api_pass',
+            default=u'BIRS API password'),
+        description=_(
+            u'help_birs_api_pass',
+            default=u'The password for the BIRS whorkshops API '
+                    u'user account.'),
+        default=None,
+        required=False)
 
     birs_location = schema.TextLine(
-        title=u'Birs location',
+        title=_(
+            u'label_birs_location',
+            default=u'BIRS events location'),
+        description=_(
+            u'help_birs_location',
+            default=u'This is CMO for production server. '
+                    u'For stagging server use EO'),
+        default=u'CMO',
         required=True,
     )
 
