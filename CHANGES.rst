4f2efef9504954cad7ce9144254faadb046efbfe
gil-cano <gil.cano@gmail.com>
2019-11-11 12:56:01 -0600

    Preparing release 1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -1,6 +1,11 @@
 Changelog
 =========
 
+1.1 (2019-05-14)
+----------------
+- Add new serach for participants
+  [adriana-rv]
+
 1.0 (2017-06-21)
 ----------------
 - remove MySQL dependency
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ long_description = '\n\n'.join([
 
 setup(
     name='cmo.events',
-    version='1.1b',
+    version='1.1',
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
