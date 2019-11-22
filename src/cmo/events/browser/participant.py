db39d7dfe2711b73562b5bb3bdebebfda2dd84eb
gil-cano <gil.cano@gmail.com>
2019-11-22 15:14:25 -0600

    participant

--- src/cmo/events/browser/participant.py
+++ src/cmo/events/browser/participant.py
@@ -8,7 +8,6 @@ from plone.dexterity.i18n import MessageFactory as _
 from plone.dexterity.interfaces import IDexterityFTI
 from plone.dexterity.utils import getAdditionalSchemata
 from plone.registry.interfaces import IRegistry
-from Products.statusmessages.interfaces import IStatusMessage
 from requests.exceptions import ConnectionError
 from requests.exceptions import HTTPError
 from z3c.form import button
@@ -46,49 +45,61 @@ class ParticipantEditView(DefaultEditForm):
             self.status = self.formErrorsMessage
             return
         self.applyChanges(data)
-        IStatusMessage(self.request).addStatusMessage(
-            self.success_message, "info"
-        )
+        api.portal.show_message(self.success_message, self.request, type=u'info')
         self.request.response.redirect(self.nextURL())
 
         birs_uri = api.portal.get_registry_record('cmo.birs_api_uri')
-        email_autho = api.portal.get_registry_record('cmo.birs_api_user')
-        passwd_autho = api.portal.get_registry_record('cmo.birs_api_password')
+        if birs_uri is None:
+            api.portal.show_message(_(u'CMO Settings: No BIRS workshops API URL defined!'), self.request, type=u'error')
+            return
+        if not valid_token():
+            api_user = api.portal.get_registry_record('cmo.birs_api_user')
+            if api_user is None:
+                api.portal.show_message(_(u'CMO Settings: No BIRS workshops API user defined!'), self.request, type=u'error')
+                return
+            api_passwd = api.portal.get_registry_record('cmo.birs_api_password')
+            if api_passwd is None:
+                api.portal.show_message(_(u'CMO Settings: No BIRS API password defined!'), self.request, type=u'error')
+                return
+            try:
+                token = requests.post(
+                    birs_uri + '/api/login.json',
+                    headers={
+                        'Accept': 'application/json',
+                        'Content-Type': 'application/json',
+                    },
+                    json={'api_user': {'email': api_user, 'password': api_passwd}},
+                )
+                token.raise_for_status()
+            except (ConnectionError, HTTPError) as err:
+                api.portal.show_message(err, self.request, type=u'error')
+                return
+            else:
+                api.portal.set_registry_record('cmo.birs_token', token.json()['jwt'])
+                api.portal.set_registry_record('cmo.token_time', time.time())
+                logger.info('We have a valid token!')
 
         try:
-            token = requests.post(
-                birs_uri + '/api/login.json',
-                headers={
-                    'Accept': 'application/json',
-                    'Content-Type': 'application/json',
-                },
-                json={'api_user': {'email': email_autho, 'password': passwd_autho}},
-            )
-
-            jwt = 'Bearer ' + token.json()['jwt']
+            jwt = 'Bearer ' + api.portal.get_registry_record('cmo.birs_token')
             workshop_number = self.context.aq_parent.id
-
             birs_url_membership = '%s/events/%s/memberships/%s.json' % (birs_uri, workshop_number, data['IMembership.birs_membership_id'])
-
             req = requests.patch(
                 birs_url_membership,
-                headers={'Accept': 'application/json', 'Content-Type': 'application/json', 'Authorization': jwt},
+                headers={'Accept': 'application/json', 'Authorization': jwt},
                 json={
                     'membership': {
                         'staff_notes': data['IMembership.event_notes'],
                         'person_attributes': {
-                            'title': data['IPerson.birs_title']
-                        }
-                    }
+                            'title': data['IPerson.birs_title'],
+                        },
+                    },
                 },
             )
             req.raise_for_status()
         except (ConnectionError, HTTPError) as err:
             api.portal.show_message(err, self.request, type=u'error')
-        # else:
-
-            # self.update_participants(req.json())
-
+        else:
+            api.portal.show_message(_(u'Updated data to BIRS'), self.request, type=u'info')
         notify(EditFinishedEvent(self.context))
 
 
