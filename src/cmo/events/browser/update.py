cec30f0dd62e09e886db4fce906d698182aae597
Gildardo Bautista García Cano <gil.cano@gmail.com>
2023-09-13 11:33:09 -0600

    update year

--- src/cmo/events/browser/update.py
+++ src/cmo/events/browser/update.py
@@ -38,7 +38,8 @@ class IMyForm(model.Schema):
 
     year = schema.Choice(
         title=_(u'Year'),
-        values=[u'2015', u'2016', u'2017', u'2018', u'2019', u'2020', u'2021', u'2022', u'2023'],
+        # values=[u'2015', u'2016', u'2017', u'2018', u'2019', u'2020', u'2021', u'2022', u'2023'],
+        values=[u'2023', u'2024'],
         required=False,
     )
 
